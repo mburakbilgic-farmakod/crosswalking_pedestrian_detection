a58bf21807a30aaf29c25234ec4d0fa4e70dc275
Canahmetozguven <100533918+Canahmetozguven@users.noreply.github.com>
2023-06-05 12:27:57 +0300

    Add files via upload
    ahö

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from src.detect_pedestrians import PedestrianDetector
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--video", type=str, default="dummy_video_1.mp4", help="Path to the video file if you want to use  webcam use 0")
-parser.add_argument("--crosswalk", type=str,  help="path to extracted video", required=False)
+parser.add_argument("--output", type=str, default ='aoutput.mp4',  help="path to extracted video")
 args = parser.parse_args()
 
-out_path = args.crosswalk
+out_path = args.output
 crosswalk_coordinates = detect_crosswalk(args.video)
 crossing_detection = PedestrianDetector(args.video, crosswalk_coordinates, out_path)
 crossing_detection.detect_pedestrian()
